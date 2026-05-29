53d8b854c892bb10477b3de6784a86e067d90ef2
Debasmita-07 <129580677+Debasmita-07@users.noreply.github.com>
2026-05-29 17:07:28 +0530

    notes project

--- form-handling/src/App.jsx
+++ form-handling/src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
-  const submitHandler = (e) =>{
+  const [first, setfirst] = useState('')
+
+  const submitHandler = (e) => {
     e.preventDefault()
-    console.log("Form submitted successfully!")
+    console.log("Form submitted successfully!", first)
+
+    setfirst('')
   }
 
   return (
     <div>
       <form action="" onSubmit={(e) => submitHandler(e)}>
-        <input type="text" placeholder='Type your text' className='bg-sky-200 rounded-sm p-4 m-10'/>
+        <input onChange={(e) => { setfirst(e.target.value) }}
+          type="text" placeholder='Type your text' className='bg-sky-200 rounded-sm p-4 m-10' value={first} />
         <button type="submit" className='bg-red-500 rounded-sm p-2'>Submit</button>
       </form>
     </div>
